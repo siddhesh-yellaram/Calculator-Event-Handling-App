957ca8bf7e0e0735a4fa59d9819efc3bdf3d30ca
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 00:56:03 +0530

    Add files via upload

---
+++ CalculatorEventHandlingApp/Model/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEventHandlingApp.Model
+{
+    public delegate void Add(Calculator obj);
+    public class Calculator
+    {
+        public event Add AddCompletion;
+        private int _x, _y, _res;
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        public int Result
+        {
+            get
+            {
+                return _res;
+            }
+        }
+        public void Add(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _res = _x + _y;
+            if (AddCompletion != null)
+            {
+                AddCompletion(this);
+            }
+        }
+    }
+}
---
+++ CalculatorEventHandlingApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorEventHandlingApp.Model;
+
+namespace CalculatorEventHandlingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            c.AddCompletion += PrintResult;
+            c.AddCompletion += GeneratePdf;
+            c.Add(15, 20);
+            Console.ReadLine();
+        }
+        static void PrintResult(Calculator obj)
+        {
+            Console.WriteLine("Addition of " + obj.X + " and " + obj.Y + " is " + obj.Result);
+        }
+
+        static void GeneratePdf(Calculator obj)
+        {
+            using (FileStream fs = new FileStream(@"D:\\test\\.NET\\AdditionRes.html", FileMode.Create))
+            {
+                using (StreamWriter st = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    st.WriteLine("<h1> Addition of " + obj.X + " and " + obj.Y + " is " + obj.Result + "</h1>");
+                }     
+            }
+        }
+    }
+}
